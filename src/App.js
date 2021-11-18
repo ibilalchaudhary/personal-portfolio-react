015cb1308595aed8b51efe43bef6ffcef2220d0d
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-11-18 18:10:01 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
